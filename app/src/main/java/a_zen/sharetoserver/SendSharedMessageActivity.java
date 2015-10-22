f00264925dd275ad202f9a49d03b182cbcde8740
alex <alex@zengers.de>
2015-10-22 22:45:03 +0200

    added shared activity

---
+++ app/src/main/java/a_zen/sharetoserver/SendSharedMessageActivity.java
@@ -0,0 +1,13 @@
+package a_zen.sharetoserver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SendSharedMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_shared_message);
+    }
+}
